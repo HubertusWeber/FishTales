7c3a13c8d82deb058653aa56c80f5e48317a293d
Hubertus Weber <1051260+HubertusWeber@users.noreply.github.com>
2024-12-24 23:06:33 +0100

    added and changed some aliases

--- alias.fish
+++ alias.fish
@@ -1,15 +1,18 @@
 # Define command aliases for convenience and efficiency.
-alias gg="lazygit"           # Shortcut for 'lazygit', a terminal UI for Git.
+alias gg="gitui"             # Shortcut for 'gitui', a terminal UI for Git.
 alias ls="eza"               # Replace 'ls' with 'eza', an enhanced version with more features.
+alias lst="eza --tree"       # Alias to list files in tree view.
 alias lsl="eza -al"          # Alias to list all files with detailed info.
 alias cdc="cd -"             # Alias to go back to the last working directory.
 alias cp="xcp -r"            # An extended `cp` (recursive by default.)
 alias vim="nvim"             # Use 'nvim' when 'vim' is called; Neovim is an improved version of Vim.
 alias e="nvim"               # Short alias for opening Neovim.
+alias th="thokr -f 3"        # Start a typing practice session with three sentences.
 alias clj="clojure -A:no-history"  # Run Clojure without recording REPL history.
 alias rm="echo Use 'rip' instead of rm."  # Prevent accidental deletion by advising to use 'rip'.
 alias rip="rip --graveyard ~/.Trash"      # 'rip' moves files to a graveyard directory instead of deleting.
 alias cat="bat --paging=never --theme=Nord" # A cat(1) clone with wings.
+alias hack="cargo hack check --feature-powerset --all-targets && cargo hack clippy --feature-powerset --all-targets" # Runs full static analysis suite: cargo check & clippy across all feature combinations and targets
 
 # Opens Home.md in Notes directory while preserving current location.
 function notes
