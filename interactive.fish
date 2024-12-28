69c8c837dd190e493a70508d7a0891f3b0a34f09
Hubertus Weber <1051260+HubertusWeber@users.noreply.github.com>
2024-12-28 20:30:15 +0100

    chagne color scheme to gruvbox-dark

--- interactive.fish
+++ interactive.fish
@@ -1,7 +1,7 @@
 # If the shell is interactive, initialize fzf, zoxide, and starship.
 if status --is-interactive
     # Set LS_COLORS environment variable using 'vivid' with 'gruvbox-dark' color scheme.
-    set -gx LS_COLORS (vivid generate gruvbox-darh)
+    set -gx LS_COLORS (vivid generate gruvbox-dark)
     # Initialize fzf key bindings and completions for fish.
     fzf --fish | source
     # Initialize zoxide, a smarter 'cd' command that learns your habits.
